b1ee7d0016e8abdae540a4b6e03499fa649bd97d
brdle <onvoidmc@gmail.com>
2022-05-12 14:15:09 -0700

    Food value fixes, attempt smore texture v1

--- src/main/java/net/brdle/delightful/common/FoodValues.java
+++ src/main/java/net/brdle/delightful/common/FoodValues.java
@@ -5,11 +5,11 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class FoodValues {
-    public static final FoodProperties CHEESEBURGER = (new FoodProperties.Builder()).nutrition(11).saturationMod(0.8F)
-            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 0), 1.0F).build();
-    public static final FoodProperties DELUXE_CHEESEBURGER = (new FoodProperties.Builder()).nutrition(11).saturationMod(0.8F)
-            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 0), 1.0F).build();
-    public static final FoodProperties MARSHMALLOW_STICK = (new FoodProperties.Builder()).nutrition(11).saturationMod(0.8F).build();
-    public static final FoodProperties COOKED_MARSHMALLOW_STICK = (new FoodProperties.Builder()).nutrition(11).saturationMod(0.8F).build();
-    public static final FoodProperties SMORE = (new FoodProperties.Builder()).nutrition(11).saturationMod(0.8F).build();
+    public static final FoodProperties CHEESEBURGER = (new FoodProperties.Builder()).nutrition(12).saturationMod(0.9F)
+            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 220, 0), 1.0F).build();
+    public static final FoodProperties DELUXE_CHEESEBURGER = (new FoodProperties.Builder()).nutrition(13).saturationMod(0.9F)
+            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 240, 0), 1.0F).build();
+    public static final FoodProperties MARSHMALLOW_STICK = (new FoodProperties.Builder()).nutrition(1).saturationMod(0.3F).build();
+    public static final FoodProperties COOKED_MARSHMALLOW_STICK = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.4F).build();
+    public static final FoodProperties SMORE = (new FoodProperties.Builder()).nutrition(7).saturationMod(0.5F).build();
 }
