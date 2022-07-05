46c93fc5fc4203b04ab94ce2566296e08cbecb87
brdle <onvoidmc@gmail.com>
2022-07-05 00:19:49 -0700

    Actually fix loot table

--- src/main/java/net/brdle/delightful/common/block/DelightfulBlocks.java
+++ src/main/java/net/brdle/delightful/common/block/DelightfulBlocks.java
@@ -2,6 +2,7 @@ package net.brdle.delightful.common.block;
 
 import net.brdle.delightful.Delightful;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -11,7 +12,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import vectorwing.farmersdelight.common.block.CabinetBlock;
-
 import java.util.function.Supplier;
 
 public class DelightfulBlocks {
@@ -19,7 +19,7 @@ public class DelightfulBlocks {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Delightful.MODID);
     public static final RegistryObject<Block> PIZZA_STONE = registerBlock("pizza_stone", () -> new PizzaStoneBlock(BlockBehaviour.Properties.of(Material.STONE).strength(0.5F, 6.0F).sound(SoundType.STONE).noOcclusion()));
     public static final RegistryObject<Block> PIZZA_PEEL = registerBlock("pizza_peel", () -> new PizzaPeelBlock(BlockBehaviour.Properties.of(Material.WOOD).strength(0.5F, 6.0F).sound(SoundType.WOOD).noOcclusion()));
-    public static final RegistryObject<Block> QUARTZ_CABINET = registerBlock("quartz_cabinet", () -> new CabinetBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.QUARTZ).requiresCorrectToolForDrops().strength(0.8F).sound(SoundType.STONE)));
+    public static final RegistryObject<Block> QUARTZ_CABINET = registerBlock("quartz_cabinet", () -> new CabinetBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.QUARTZ).requiresCorrectToolForDrops().strength(0.8F)));
     public static RegistryObject<Block> registerBlock(String name, Supplier<Block> block) {
         return BLOCKS.register(name, block);
     }
