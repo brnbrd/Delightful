c8b71f32cf2d145bc4c1f5e2c9f1fa1470f0e5f9
brnbrd <onvoidmc@gmail.com>
2023-04-12 08:17:09 -0700

    Fix cheese tag

--- src/main/java/net/brnbrd/delightful/data/gen/DelightfulItemTagProvider.java
+++ src/main/java/net/brnbrd/delightful/data/gen/DelightfulItemTagProvider.java
@@ -189,7 +189,6 @@ public class DelightfulItemTagProvider extends ItemTagsProvider {
 			.addOptional(Util.rl("quark", "cooked_crab_leg"));
 		this.tag(DelightfulItemTags.CHEESE)
 			.addOptional(Util.rl("brewinandchewin", "flaxen_cheese_wedge"))
-			.addOptional(Util.rl("brewinandchewin", "scarlet_cheese_wedge"))
 			.addOptional(Util.rl("farmlife", "tribull_cheese_wedge"))
 			.addOptional(Util.rl("croptopia", "cheese"));
 		this.tag(ForgeTags.MILK)
